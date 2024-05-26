7bfbc05b3a5166df8cdfe5986918def11db84ac5
vinhkim1409 <kimxuanvinh2002@gmail.com>
2024-05-26 16:16:31 +0700

    fix cors

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 dotenv.config();
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: true,
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
