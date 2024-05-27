66148fddbb08c723236cf00a02be9fae0a90d573
vinhkim1409 <kimxuanvinh2002@gmail.com>
2024-05-27 15:32:59 +0700

    fix payment notify

--- server/src/routes/paymentRoutes.js
+++ server/src/routes/paymentRoutes.js
@@ -5,5 +5,5 @@ const router = express.Router();
 const MonoController = require("../controllers/MomoController")
 router.post("/payMomo",authMiddlewares.verifyTokenCustomer,MonoController.payOrderMomo)
 router.post("/payPackage",authMiddlewares.verifyToken,authMiddlewares.verifyAdminAuth,MonoController.payPackageMomo)
-
+router.post("/notify_payment_package",MonoController.packageNotification)
 module.exports = router;
